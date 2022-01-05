c03c5a31479a9f7df9720791085aa291aa1387bb
Ahn-Gihwan <ahngh0113@naver.com>
2022-01-06 00:33:28 +0900

    refact: hook을 이용하여 preload하는 방법으로 변환
    * 다른 서버에 있는 데이터를 가져오는 Image.prefetch()는 hooks이 따로 없다.

--- App.js
+++ App.js
@@ -1,39 +1,15 @@
 import React, { useState } from "react";
 import AppLoading from "expo-app-loading";
 import { Text, View, Image } from "react-native";
-import * as Font from "expo-font";
+import { useFonts } from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
-import { Asset } from "expo-asset";
-
-const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
-const loadAssets = (images) =>
-  images.map((image) => {
-    if (typeof image === "string") return Image.prefetch(image);
-    else return Asset.loadAsync(image);
-  });
+import { Asset, useAssets } from "expo-asset";
 
 export default function App() {
-  const [ready, setReady] = useState(false);
+  const [assets] = useAssets([require("./bg.jpeg")]);
+  const [loaded] = useFonts(Ionicons.font);
 
-  const startLoading = async () => {
-    const fonts = loadFonts([Ionicons.font]);
-    const images = loadAssets([
-      require("./bg.jpeg"),
-      "https://d33wubrfki0l68.cloudfront.net/b152eb4214943f96e83c4babde026b12221e68f1/a20c2/img/oss_logo.png",
-    ]);
-
-    await Promise.all([...fonts, ...images]);
-  };
-  const onFinish = () => setReady(true);
-
-  if (!ready)
-    return (
-      <AppLoading
-        startAsync={startLoading}
-        onFinish={onFinish}
-        onError={console.log}
-      />
-    );
+  if (!assets || !loaded) return <AppLoading />;
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
