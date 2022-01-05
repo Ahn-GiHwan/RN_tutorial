da3b031dc334bfe6fe90ff98a2d0fb096ad08eee
Ahn-Gihwan <ahngh0113@naver.com>
2022-01-06 02:23:17 +0900

    feat: Use DarkTheme, DefaultTheme of @react-navigation/native
    NavigationContainer Component에 props로 넘겨주면 mode에 맞게 자동으로 색상을 변경해준다.

--- App.js
+++ App.js
@@ -3,17 +3,24 @@ import AppLoading from "expo-app-loading";
 import { useFonts } from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
 import { useAssets } from "expo-asset";
-import { NavigationContainer } from "@react-navigation/native";
+import {
+  NavigationContainer,
+  DarkTheme,
+  DefaultTheme,
+} from "@react-navigation/native";
 import Tabs from "./navigation/Tabs";
+import { useColorScheme } from "react-native";
 
 export default function App() {
   const [assets] = useAssets([require("./bg.jpeg")]);
   const [loaded] = useFonts(Ionicons.font);
 
+  const isDark = useColorScheme() === "dark";
+
   if (!assets || !loaded) return <AppLoading />;
 
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={isDark ? DarkTheme : DefaultTheme}>
       <Tabs />
     </NavigationContainer>
   );
