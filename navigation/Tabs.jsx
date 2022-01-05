a605457f6d1c9dfe489f13afcfa0df259210e32e
Ahn-Gihwan <ahngh0113@naver.com>
2022-01-06 02:01:02 +0900

    feat: appy screenOptions and options

--- navigation/Tabs.jsx
+++ navigation/Tabs.jsx
@@ -3,15 +3,52 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Movies from "../screens/Movies";
 import Tv from "../screens/Tv";
 import Search from "../screens/Search";
+import { Button, Text } from "react-native";
 
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Movies" component={Movies} />
-      <Tab.Screen name="Tv" component={Tv} />
-      <Tab.Screen name="Search" component={Search} />
+    <Tab.Navigator
+      screenOptions={{
+        tabBarLabelStyle: {
+          color: "black",
+        },
+        tabBarActiveTintColor: "pink",
+        tabBarStyle: {
+          backgroundColor: "skyblue",
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Movies"
+        component={Movies}
+        options={{
+          tabBarLabelStyle: {
+            color: "red",
+          },
+          tabBarBadge: 3,
+          headerLeft: () => (
+            <Button
+              title="<"
+              onPress={() => {
+                alert("Go Back!");
+              }}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen name="TV" component={Tv} />
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarBadge: "NEW",
+          headerStyle: {
+            backgroundColor: "red",
+          },
+        }}
+      />
     </Tab.Navigator>
   );
 };
