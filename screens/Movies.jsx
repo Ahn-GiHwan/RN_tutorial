ab4d09ee8f75a79aeb97ef7381747593ae92c90c
Ahn-Gihwan <ahngh0113@naver.com>
2022-01-06 15:23:28 +0900

    style: use styled-components

--- screens/Movies.jsx
+++ screens/Movies.jsx
@@ -1,16 +1,25 @@
 import React from "react";
+import styled from "styled-components/native";
 import { Text, TouchableOpacity, View } from "react-native";
+
+const Btn = styled.Text`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: red;
+`;
+
+const Title = styled.Text`
+  color: blue;
+  font-size: 20px;
+  font-weight: 800;
+`;
 
 const Movies = ({ navigation: { navigate } }) => {
   return (
-    <TouchableOpacity
-      onPress={() => navigate("Stack", { screen: "Three" })}
-      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-    >
-      {/* <View > */}
-      <Text>Movie</Text>
-      {/* </View> */}
-    </TouchableOpacity>
+    <Btn onPress={() => navigate("Stack", { screen: "Three" })}>
+      <Title>Movie</Title>
+    </Btn>
   );
 };
 
